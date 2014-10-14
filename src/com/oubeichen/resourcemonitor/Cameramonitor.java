7982f677d558d8cf3d600e33e741b41cebc70f0f
oubeichen <oubeichen@gmail.com>
2014-10-14 15:39:06 +0800

    Add a simple cameramonitor

---
+++ src/com/oubeichen/resourcemonitor/Cameramonitor.java
@@ -0,0 +1,37 @@
+package com.oubeichen.resourcemonitor;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class Cameramonitor implements IXposedHookLoadPackage {
+	
+	//http://developer.android.com/reference/android/hardware/Camera.html
+
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+
+        findAndHookMethod("android.hardware.Camera", lpparam.classLoader, "open", new XC_MethodHook() {
+        	@Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+        		XposedBridge.log("Started camera: " + lpparam.packageName);
+            }
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		XposedBridge.log("Started camera2: " + lpparam.packageName);
+            }
+        });
+        
+        findAndHookMethod("android.hardware.Camera", lpparam.classLoader, "release", new XC_MethodHook() {
+        	@Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+        		XposedBridge.log("Stopped camera: " + lpparam.packageName);
+            }
+        	@Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+        		XposedBridge.log("Stopped camera2: " + lpparam.packageName);
+            }
+        });
+    }
+}	
