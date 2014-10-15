64248718c3cf3c94641cf0824ffb7afd2642f7d4
oubeichen <oubeichen@gmail.com>
2014-10-15 17:11:37 +0800

    change the layout of main activity

--- src/com/oubeichen/resourcemonitor/MainActivity.java
+++ src/com/oubeichen/resourcemonitor/MainActivity.java
@@ -1,18 +1,57 @@
 package com.oubeichen.resourcemonitor;
 
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	ListView mListView;
+	Button clearBtn; 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mListView=new ListView(this);
+		selectDB();
+		btnInit();
+		
+	}
+    
+	public void btnInit(){
+		clearBtn = (Button)findViewById(R.id.clear_button);
+		clearBtn.setOnClickListener(this);
 	}
 
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+		case R.id.clear_button:
+			DatabaseHelper dbHelper = new DatabaseHelper(ContextUtil.getInstance());
+			dbHelper.clear();
+			break;
+		}
+	}
+
+    public void selectDB(){
+    	DatabaseHelper dbHelper = new DatabaseHelper(ContextUtil.getInstance());
+
+    	//camera usage
+    	List<String> list = dbHelper.selectAll("camerausage");
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,list);
+        mListView.setAdapter(adapter);
+    }
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -31,4 +70,5 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
