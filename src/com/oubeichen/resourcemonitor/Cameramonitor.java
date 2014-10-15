418845564b09b7459136b746bbd8cbd4447e3c6d
oubeichen <oubeichen@gmail.com>
2014-10-15 11:51:34 +0800

    Add debug log

--- src/com/oubeichen/resourcemonitor/Cameramonitor.java
+++ src/com/oubeichen/resourcemonitor/Cameramonitor.java
@@ -1,6 +1,9 @@
 package com.oubeichen.resourcemonitor;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -12,7 +15,18 @@ public class Cameramonitor implements IXposedHookLoadPackage {
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
 
-        findAndHookMethod("android.hardware.Camera", lpparam.classLoader, "open", new XC_MethodHook() {
+    	Class<?> hookClass = null;
+    	try{
+    		hookClass = findClass("android.hardware.Camera", lpparam.classLoader);
+    	}catch (Throwable ex) {
+    		XposedBridge.log("Class not found");
+
+		}
+    	for (Method method : hookClass.getDeclaredMethods()){
+    		XposedBridge.log(method.getName());
+    	}
+    	Method open = hookClass.getDeclaredMethod("open");
+    	XposedBridge.hookMethod(open, new XC_MethodHook() {
         	@Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
         		long time=System.currentTimeMillis();
@@ -24,8 +38,8 @@ public class Cameramonitor implements IXposedHookLoadPackage {
         		XposedBridge.log("Started camera2: " + lpparam.packageName  + " at time: " + time);
             }
         });
-        
-        findAndHookMethod("android.hardware.Camera", lpparam.classLoader, "release", new XC_MethodHook() {
+    	Method release = hookClass.getDeclaredMethod("release");
+    	XposedBridge.hookMethod(release, new XC_MethodHook() {
         	@Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
         		long time=System.currentTimeMillis();
